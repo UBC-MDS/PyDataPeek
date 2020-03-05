24cf2e56214a25b5bb2aa37649353f9ed9108618
wongmon <wong.monique.mc@gmail.com>
2020-03-04 21:26:20 -0800

    Add finished function, #18

--- PyDataPeek/sample_data.py
+++ PyDataPeek/sample_data.py
@@ -1,43 +1,107 @@
 import pandas as pd
+import numpy as np
 
+def read_file(file, sheet_name=0):
+     """Helper function used to read the file and return a pandas dataframe.
 
-def sample_data(file, sheet_name=0, dir=''):
-     """Return a data table showing column names in rows, an example record, column data types and
-        summary statistics for numerical, categorical and long-form text data
-     
+     Checks if file type is a .csv or excel. If not, returns a ValueError.
+
      Parameters
      ----------
      file : str
          the name of the file, including the filetype extension
      sheet_name : int, optional
          if passing an excel file, the name of the sheet to analyze, by default 0
-     dir : str, optional
-         the directory where the file should be saved, by default ''
-     
+
      Returns
      -------
-     .csv file
-        data table showing data summary, as a .csv file
-        data summary includes column names in rows, an example record, column data types and
-        summary statistics for numerical (range), categorical (unique values) 
-        and long-form text data (average length of string)
+     pandas.Dataframe
+         pandas dataframe containing data from file
 
-     
      Example
      -------
-     >>> sample_data(customers.xlsx, sheet_name='2019', dir='report')
+    >>> read_file(customers.xlsx, sheet_name='2019')
      """
-     # Check filetype and read file if valid
      if file.endswith('.csv'):
          df = pd.read_csv(file)
      else:
          try:
              df = pd.read_excel(file, sheet_name=sheet_name)
          except:
-             print("Please use a valid csv or excel file.")
+             raise ValueError("Please use a valid csv or excel file.")
+     return df
 
-     # Create dataframe of data summary
 
-     # Save and return csv
-     # csv = pd.DataFrame.to_csv(summary)
-     # return csv
+def summarize_data(df):
+    """Helper function that returns  data table showing column names in rows, an example record, column data types
+    and summary statistics for numerical, text data
+    
+    Parameters
+    ----------
+     df : pandas.DataFrame
+         the dataframe object to analyze
+     
+    Returns
+    -------
+    .csv file
+        data table showing data summary, as a .csv file
+        data summary includes column names in rows, an example record, column data types and
+        summary statistics for integer, string and float data
+
+     
+    Example
+    -------
+    >>> summarize_data(df)
+    """
+    ## add record sample
+    results = pd.DataFrame({'sample_record': df.iloc[1]})
+
+    ## add data types
+    results['data_type'] = df.dtypes
+
+    ## add summary statistics based on data type
+    summary = []
+    for index, row in results.iterrows():
+        if row['data_type'] == 'int64':
+            summary.append("unique values: " + str(df[index].unique().size))
+        elif row['data_type'] == 'object':
+            summary.append("average length of string: " + str(round(np.mean([len(str(i)) for i in df[index]]), 1)))
+        elif row['data_type'] == 'float64':
+            summary.append("median value: " + str(np.median(df[index])))
+        else:
+            summary.append("No summary available")      
+    results['summary'] = summary
+
+
+    # save and return csv
+    csv = results.to_csv()
+    return csv
+
+
+def sample_data(file, sheet_name=0)::
+    """Returns  data table showing column names in rows, an example record, column data types
+    and summary statistics for numerical, text data from Excel or csv data
+    
+     Parameters
+     ----------
+     file : str
+         the name of the file, including the filetype extension
+     sheet_name : int, optional
+         if passing an excel file, the name of the sheet to analyze, by default 0
+     
+    Returns
+    -------
+    .csv file
+        data table showing data summary, as a .csv file
+        data summary includes column names in rows, an example record, column data types and
+        summary statistics for integer, string and float data
+
+     
+    Example
+    -------
+    >>> sample_datacustomers.xlsx, sheet_name='2019')
+    """
+
+    df = read_file(file, sheet_name)
+    csv = summarize_data(df)
+    return csv
