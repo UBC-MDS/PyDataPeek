b55815130de61cb0ac4249e37edfff5abdf16352
Miro (Shangjing) HU <mirohu@outlook.com>
2020-03-09 15:07:14 -0700

    fixed minor in explore_w_histogram.py, related to issue #28

--- PyDataPeek/explore_w_histograms.py
+++ PyDataPeek/explore_w_histograms.py
@@ -2,6 +2,7 @@ import pandas as pd
 import altair as alt
 from altair_saver import save
 
+
 def read_file(file, sheet_name=0):
     """
     Helper function used to read the file and return a pandas dataframe.
@@ -26,83 +27,90 @@ def read_file(file, sheet_name=0):
             raise ValueError("Please use a valid csv or excel file.")
     return df
 
+
 def is_numeric(df, column):
     """
     Helper function used to take a dataframe, a column name
     Returns True if the column is numerical, False otherwise
-    
+
     Parameters
     --------
     file:
         a csv file
     column: 
         a column name as a string
-    
-    
+
+
     Returns
     -------
     Bool
     """
- 
+
     if str(df[column].dtypes) == 'int64' or str(df[column].dtypes) == 'float64':
         return True
     else:
-        return False       
-    
+        return False
+
+
 def make_save_histogram(df, column):
     """
     Helper function used to take a dataframe, a numerical column name, 
     and returns a png file of histogram(s)
-    
+
     Parameters
     --------
     file:
         a csv file
     column: 
         a numerical column name as a string
-    
-    
+
+
     Returns
     -------
     .png file of histogram
-    """     
+    """
 
     # plot and save the chart with different name
     chart = alt.Chart(df).mark_bar().encode(
-                alt.X(column+':Q', bin = True),
-                alt.Y('count()')
-            )
+        alt.X(column+':Q', bin=True),
+        alt.Y('count()')
+    )
 
     chart.save(column + '_chart' + '.png')
-        
-def explore_w_histograms(file, columns_list, sheet_name =0):
+
+
+def explore_w_histograms(file, columns_list=[], sheet_name=0):
     """
     take a csv file, a sheet name (default 0),
     a list of numerical column names 
     and returns a png file of histogram(s)
-    
+
     Parameters
     --------
     file:
         a csv file
     columns_list: 
-        a list of numerical column names as strings
-    
-    
+        a list of numerical column names as strings, default = []
+
+
     Returns
     -------
     .png file(s) of histogram(s)
-    
+
     Example
     -------
-    >>> explore_w_histograms(df, ['volumn', 'date'])
+    >>> explore_w_histograms('data/toy_dataset.csv', ['volumn', 'date'])
     """
-    df = read_file(file, sheet_name = 0)
-            
+    df = read_file(file, sheet_name=0)
+
+    try:
+        column = columns_list[0]
+    except:
+        raise ValueError("Make sure column name is in your data!")
+
     for i in range(0, len(columns_list)):
         if is_numeric(df, columns_list[i]) == True:
             make_save_histogram(df, columns_list[i])
         else:
-            print(str(df[columns_list[i]]), "is not a numerical column. Please use numerical column only.")
-
-    
+            print(str(df[columns_list[i]]),
+                  "is not a numerical column. Please use numerical column only.")
