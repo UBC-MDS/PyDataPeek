04454fdd3c77b8e78e48cb3d3dc855a52efb5bf4
Miro (Shangjing) HU <mirohu@outlook.com>
2020-03-13 11:40:26 -0700

    updated _init_ file

--- PyDataPeek/__init__.py
+++ PyDataPeek/__init__.py
@@ -1,1 +1,15 @@
 __version__ = '0.1.6'
+
+import pandas as pd
+import altair as alt
+import matplotlib.pyplot as plt
+import os
+from xlrd import XLRDError
+import seaborn as sns
+from wordcloud import WordCloud, STOPWORDS
+
+from PyDataPeek import *
+from PyDataPeek.missing_data_overview import *
+from PyDataPeek.sample_data import *
+from PyDataPeek.explore_w_histograms import *
+from PyDataPeek.word_bubble import *
